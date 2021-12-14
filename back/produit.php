0dc4e2c952699996c9d0acec3f52c25ad224854e
HASSAN Adam <adamhassanpro@gmail.com>
2021-12-14 18:33:17 +0100

    ok ok

--- back/produit.php
+++ back/produit.php
@@ -393,7 +393,7 @@ if($Valider)
                <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modification</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Modifications</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
